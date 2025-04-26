41c07469efc7c1124e7c2ae95281d38ba77bef94
Kim <k-hmerk@outlook.com>
2025-04-26 14:43:04 +0200

    adding a lot of stuff

--- init.el
+++ init.el
@@ -7,8 +7,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87" default))
  '(package-selected-packages
-   '(nix-mode all-the-icons-dired sudo-edit magit org-bullets which-key)))
+   '(gruvbox-theme gruvbox sly nixos-options nix-mode all-the-icons-dired sudo-edit magit org-bullets which-key))
+ '(warning-suppress-types '((comp) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
