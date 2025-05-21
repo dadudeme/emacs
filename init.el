1c3e8be9cc8728ba1ce5c15001b0f5de0fbf3767
Kim <k-hmerk@outlook.com>
2025-05-21 16:24:28 +0200

    Ficxing a lot of annoying things

--- init.el
+++ init.el
@@ -7,11 +7,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("d5fd482fcb0fe42e849caba275a01d4925e422963d1cd165565b31d3f4189c87" default))
  '(package-selected-packages
-   '(gruvbox-theme gruvbox sly nixos-options nix-mode all-the-icons-dired sudo-edit magit org-bullets which-key))
- '(warning-suppress-types '((comp) (comp))))
+   '(all-the-icons-dired all-the-icons-ivy-rich corfu counsel direnv elpy
+			 gruvbox-theme magit nix-mode nixos-options
+			 org-auto-tangle org-bullets org-tempo sly
+			 sudo-edit which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
